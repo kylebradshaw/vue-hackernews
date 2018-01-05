18d70240502f8390f23ec7d751920b482807f6bf
eddyerburgh <edward.yerburgh@gmail.com>
2018-01-05 07:08:39 +0000

    test: remove debugger statement

--- src/components/__test__/Item.spec.js
+++ src/components/__test__/Item.spec.js
@@ -4,7 +4,6 @@ import Item from '../Item.vue'
 describe('Item.vue', () => {
   test('renders item', () => {
     const wrapper = mount(Item)
-    debugger
     expect(wrapper.text()).toContain('item') // #A
   })
 })
