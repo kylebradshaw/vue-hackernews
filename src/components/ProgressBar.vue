681762e1a63a78efaf7c72eab60e5081f424d846
eddyerburgh <edward.yerburgh@gmail.com>
2018-01-05 19:36:59 +0000

    test: add ProgressBar tests

--- src/components/ProgressBar.vue
+++ src/components/ProgressBar.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class="progress" :style="{
-    'width': `${percent}%` //#A
+  <div class="progress"
+    :class="{
+    show: show,
+    error: error
+  }"
+  :style="{
+    'width': `${percent}%`
   }"></div>
 </template>
 
@@ -8,7 +13,23 @@
 export default {
   name: 'progress-bar',
   data: () => ({
-    percent: 0
-  })
+    percent: 0,
+    error: false,
+    show: false
+  }),
+  methods: {
+    start () {
+      this.show = true
+      this.percent = 0
+    },
+    finish () {
+      this.show = false
+      this.percent = 100
+    },
+    fail () {
+      this.error = true
+      this.percent = 100
+    }
+  }
 }
 </script>
--- src/components/__tests__/ProgressBar.spec.js
+++ src/components/__tests__/ProgressBar.spec.js
@@ -1,21 +1,65 @@
-import { mount } from 'vue-test-utils'
+import { shallow } from 'vue-test-utils'
 import ProgressBar from '../ProgressBar.vue'
 
 describe('ProgressBar.vue', () => {
-  test('has no show class onn initial render', () => {
-    const wrapper = mount(ProgressBar)
+  test('has no show class on initial render', () => {
+    const wrapper = shallow(ProgressBar)
     expect(wrapper.classes()).not.toContain('show') // #A
   })
 
   test('initializes with 0% width', () => {
-    const wrapper = mount(ProgressBar)
+    const wrapper = shallow(ProgressBar)
     expect(wrapper.element.style.width).toBe('0%') // #A
   })
 
   test('sets .progress width as same value of vm.percentage', () => {
-    const wrapper = mount(ProgressBar)
+    const wrapper = shallow(ProgressBar)
     const percent = 70
     wrapper.setData({ percent }) // #A
     expect(wrapper.element.style.width).toBe(`${percent}%`)// #B
   })
+
+  test('renders element with width 0% when start is called', () => {
+    const wrapper = shallow(ProgressBar)
+    wrapper.setData({ percent: 70 }) // #A
+    wrapper.vm.start()
+    wrapper.update()
+    expect(wrapper.element.style.width).toBe('0%')
+  })
+
+  test('adds class show when start is called', () => {
+    const wrapper = shallow(ProgressBar)
+    wrapper.vm.start()
+    wrapper.update()
+    expect(wrapper.classes()).toContain('show')
+  })
+
+test('adds class error when fail is called', () => {
+  const wrapper = shallow(ProgressBar)
+  wrapper.vm.fail()
+  wrapper.update()
+  expect(wrapper.classes()).toContain('error')
+})
+
+test('renders element with width 100% when fail is called', () => {
+  const wrapper = shallow(ProgressBar)
+  wrapper.vm.fail()
+  wrapper.update()
+  expect(wrapper.element.style.width).toBe('100%')
+})
+
+test('removes class show when finish is called', () => {
+  const wrapper = shallow(ProgressBar)
+  wrapper.vm.start()
+  wrapper.vm.finish()
+  wrapper.update()
+  expect(wrapper.classes()).not.toContain('show')
+})
+
+test('sets width to 100% when finish is called ', () => {
+  const wrapper = shallow(ProgressBar)
+  wrapper.vm.finish()
+  wrapper.update()
+  expect(wrapper.element.style.width).toBe('100%')
+})
 })
