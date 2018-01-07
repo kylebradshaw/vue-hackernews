69f95045688f3fe0e4a547a420c3afb57881d6cc
eddyerburgh <edward.yerburgh@gmail.com>
2018-01-07 17:57:48 +0000

    style: add style to ProgressBar

--- src/components/ProgressBar.vue
+++ src/components/ProgressBar.vue
@@ -12,3 +12,24 @@ export default {
   })
 }
 </script>
+
+<style scoped>
+.progress {
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  right: 0px;
+  height: 2px;
+  width: 0%;
+  transition: width 0.2s, opacity 0.4s;
+  opacity: 0;
+  background-color: #ffca2b;
+  z-index: 999999;
+}
+.show {
+  opacity: 1;
+}
+.error {
+  background-color: #ff0000;
+}
+</style>
