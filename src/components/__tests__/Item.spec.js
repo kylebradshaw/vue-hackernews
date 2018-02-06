bf159d156e7a03b9ed3f88e69a39643e98c403f9
Kyle Bradshaw <kyle.bradshaw@digitas.com>
2018-02-06 13:13:38 -0500

    item spec testing vue component

--- src/components/__tests__/Item.spec.js
+++ src/components/__tests__/Item.spec.js
@@ -1,3 +1,10 @@
-test('sanity test', () => {
-  return
+import Item from '../Item.vue'
+import Vue from 'vue'
+
+describe('Item.vue', () => {
+  test('renders item', () => {
+    const Ctor = Vue.extend(Item)
+    const vm = new Ctor().$mount()
+    expect(vm.$el.textContent).toContain('item')
+  })
 })
