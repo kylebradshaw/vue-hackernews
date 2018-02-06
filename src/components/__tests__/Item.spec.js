eae762e8cca91edcf35c0d8a65acf64df3717fff
Kyle Bradshaw <kyle.bradshaw@digitas.com>
2018-02-06 14:05:56 -0500

    vue-test-utils implementation

--- src/components/__tests__/Item.spec.js
+++ src/components/__tests__/Item.spec.js
@@ -1,14 +1,25 @@
-import Item from '../Item.vue'
-import Vue from 'vue'
+// import Item from '../Item.vue'
+// import Vue from 'vue'
 
-describe('Item.vue', () => {
-  test('renders item', () => {
-    const Ctor = Vue.extend(Item) // component
-    const vm = new Ctor().$mount() // mount component
-    expect(vm.$el.textContent).toContain('item')
+// describe('Item.vue', () => {
+//   test('renders item', () => {
+//     const Ctor = Vue.extend(Item) // component
+//     const vm = new Ctor().$mount() // mount component
+//     expect(vm.$el.textContent).toContain('item')
+//   })
+// })
+
+// // Now we’ve got our first component test running.
+// // As you can see, mounting a component requires quite a bit of code.
+// // Luckily there’s an official Vue testing library that mounts components for you.In the next section, we’ll use vue - test - utils to simplify the test code in Item.spec.js.
+
+// REDO with vue-test-utils
+import { mount } from 'vue-test-utils'
+import Item from '../Item.vue'
+
+describe.only('Item.vue', () => {
+  test('should render an item', () => {
+    const wrapper = mount(Item)
+    expect(wrapper.vm.$el.textContent).toContain('item')
   })
 })
-
-// Now we’ve got our first component test running.
-// As you can see, mounting a component requires quite a bit of code.
-// Luckily there’s an official Vue testing library that mounts components for you.In the next section, we’ll use vue - test - utils to simplify the test code in Item.spec.js.
