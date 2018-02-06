b3b669972c0df4c3d431393767859e207516b608
Kyle Bradshaw <kyle.bradshaw@digitas.com>
2018-02-06 13:31:46 -0500

    context

--- src/components/__tests__/Item.spec.js
+++ src/components/__tests__/Item.spec.js
@@ -3,8 +3,12 @@ import Vue from 'vue'
 
 describe('Item.vue', () => {
   test('renders item', () => {
-    const Ctor = Vue.extend(Item)
-    const vm = new Ctor().$mount()
+    const Ctor = Vue.extend(Item) // component
+    const vm = new Ctor().$mount() // mount component
     expect(vm.$el.textContent).toContain('item')
   })
 })
+
+// Now we’ve got our first component test running.
+// As you can see, mounting a component requires quite a bit of code.
+// Luckily there’s an official Vue testing library that mounts components for you.In the next section, we’ll use vue - test - utils to simplify the test code in Item.spec.js.
